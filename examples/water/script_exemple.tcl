dacee05ec81f1d91d5e9d601c62f55bde27e0407
ClaireLoison <claire.loison@univ-lyon1.fr>
2020-01-17 15:13:39 +0100

    small modif in script_example.tcl

--- examples/water/script_exemple.tcl
+++ examples/water/script_exemple.tcl
@@ -1,9 +1,18 @@
-mol new water_pure1.psf
-mol addfile water_pure.1.skip1000.dcd waitfor all
+# you can use this file by typing in a shell 
+# vmd -dispdev text -e script_exemple.tcl
+#######################
+#=== preparations ! change the path to the path of the tcl package !
+
+source ~/Calculations/TOOLS/my_vmd_density_profile/orientation_profile.tcl
 
 proc pptable5file {file l1 l2 l3 l4 l5 } { foreach i1 $l1 i2 $l2  i3 $l3 i4 $l4  i5 $l5  { puts $file " [format %10.6f $i1]\t[format %10.6f $i2] \t[format %10.6f $i3] \t[format %10.6f $i4] \t[format %10.6f $i5]" }  }
 
-source ~/Calculations/TOOLS/vmd_density_profile/orientation_profile.tcl
+#=== loading trajectory data from NAMD calculation
+
+mol new water_pure1.psf
+mol addfile water_pure.1.skip1000.dcd waitfor all
+
+#=== analyse and output
 
 set wdir [density_dir_profile -rho direction -selection "water and resid 11 to 20" -average 1 -frame_from 18 -frame_to 19 -axisDef "water"]
 
